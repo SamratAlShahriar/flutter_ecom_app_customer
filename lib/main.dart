69b0d7654353375a06287834a7a1de424bede3af
samratalshahriar <samratalshahriar@gmail.com>
2022-11-13 01:53:48 +0600

    init app

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {},
+    );
   }
 }
